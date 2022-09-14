d3c6486ab67745ed9c11c03f39d88bf5e94fbff3
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-14 22:28:00 +0600

    fixed count overlay problem

--- script.js
+++ script.js
@@ -114,8 +114,8 @@ const start = () => {
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
-      document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.addEventListener("keydown", typeController);
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
